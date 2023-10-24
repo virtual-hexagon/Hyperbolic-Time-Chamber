87c3d1aef25e0d190dbcb41fea2d25681a58846d
Murray Leigh <2942013+virtual-hexagon@users.noreply.github.com>
2023-10-24 00:42:47 -0700

    Corrected some calculations, cleaned up code, and added JSON data logging

--- exercises/numeric-methods/understanding-numeric-error/src/numeric_derivative.cpp
+++ exercises/numeric-methods/understanding-numeric-error/src/numeric_derivative.cpp
@@ -1,8 +1,11 @@
-#include <array>     //std::array
-#include <algorithm> //std::for_each
-#include <iostream>  //std::{cout, ostream_iterator}
-#include <numeric>   //std::iota
-#include <ranges>    //std::{ranges, views}
+#include <array>      //std::array
+#include <algorithm>  //std::{for_each, generate]
+#include <filesystem> // use for creating directory
+#include <fstream>    //std::ofs filestream
+#include <iostream>   //std::{cout, ostream_iterator}
+#include <numeric>    //std::iota
+#include <ranges>     //std::{ranges, views}
+#include "json.hpp"   //include json library
 
 
 // Define a function that we want to examine
@@ -11,14 +14,14 @@
 //   so we can measure the accuracy of our numeric approximation of the derivative
 double Fx( const double &x ){
     using namespace std;
-    return ( (1+x*sin(x)) / pow(x,3) );
+    return ( (1+x*sin(x)) / (pow(x,3)) );
 }
 
 // Define the true analytic derivative of the function
 // that was computed by hand
 double derivFx ( const double &x ){
     using namespace std;
-    return ( (pow(x,2)*cos(x)-2*x*sin(x)-3) / ( pow(x,4) ) );
+    return ( (pow(x,2)*cos(x)-2*x*sin(x)-3) / (pow(x,4)) );
 }
 
 int main() {
@@ -40,23 +43,38 @@ int main() {
 
     // Note that the transform is comparable to:
     //>`std::copy(powers.begin(),powers.end(),grids.begin());`
-    //>`std::for_each(grids.begin(),grids.end(), [](auto &x) { x = std::pow(2,x); });`
-    std::ranges::transform(powers, grids.begin(),[](auto x) { return std::pow(2,x); });
+    //>`std::for_each(grids.begin(),grids.end(), [](auto &x) { x = std::pow(2,-x); });`
+    std::ranges::transform(powers, grids.begin(),[](auto &x)->double{ return std::pow(2,-x); });
 
     // Define some strategies for approximating derivatives using
-    // Finite Difference Approximations
+    // Finite Difference Approximations and apply those to our data
     // Notes:
     // - 1st order forward difference
     // - 2nd order central difference
     // - 4th order central difference
-    auto forwardDiffFun  = [&x](auto h){ return Fx(x+h)-Fx(x)   /   h; };
-    auto centralDiffFun  = [&x](auto h){ return Fx(x+h)-Fx(x-h) / 2*h; }; 
-    auto centralDiff4Fun = [&x](auto h){ return Fx(x-2*h)-8*Fx(x-h)+8*Fx(x+h)-Fx(x+2*h) / 12*h; };
+    auto forwardDiffFun  = [&x](auto &h)->double{ 
+        double num = Fx(x+h)-Fx(x);
+        double den = h;
+        return num / den;
+    };
+    auto centralDiffFun  = [&x](auto &h)->double{ 
+        double num = Fx(x+h)-Fx(x-h);
+        double den = 2*h;
+        return num / den;
+    }; 
+    auto centralDiff4Fun = [&x](auto &h)->double{   
+        double num = Fx(x-2*h)-8*Fx(x-h)+8*Fx(x+h)-Fx(x+2*h);
+        double den = 12*h;
+        return num / den;
+    };
+    
+    auto forwardDiff  = grids | std::ranges::views::transform(forwardDiffFun);
+    auto centralDiff  = grids | std::ranges::views::transform(centralDiffFun);
+    auto centralDiff4 = grids | std::ranges::views::transform(centralDiff4Fun);
 
-    auto trueDerivative = grids | std::ranges::views::transform(derivFx);
-    auto forwardDiff    = grids | std::ranges::views::transform(forwardDiffFun);
-    auto centralDiff    = grids | std::ranges::views::transform(centralDiffFun);
-    auto centralDiff4   = grids | std::ranges::views::transform(centralDiff4Fun);
+    // Compute the true analytic derivative value @ x
+    std::array<double, data_size_> trueDerivative;
+    std::ranges::fill(trueDerivative, derivFx(x));
 
     // Compute the error by taking the difference between the analytic solution and the approximation
     std::array<double, data_size_> forwardDiffError;
@@ -66,15 +84,47 @@ int main() {
     std::ranges::transform(trueDerivative, forwardDiff,  forwardDiffError.begin(), std::minus{});
     std::ranges::transform(trueDerivative, centralDiff,  centralDiffError.begin(), std::minus{});
     std::ranges::transform(trueDerivative, centralDiff4, centralDiff4Error.begin(),std::minus{});
+    
+    auto absFun = [](auto &x)->double{ return std::abs(x); };
+    std::ranges::transform(forwardDiffError,  forwardDiffError.begin(), absFun);
+    std::ranges::transform(centralDiffError,  centralDiffError.begin(), absFun);
+    std::ranges::transform(centralDiff4Error, centralDiff4Error.begin(),absFun);
 
-    auto absFun = [](auto x){ return std::abs(x); };
-    std::ranges::transform(forwardDiffError, forwardDiffError.begin(), absFun);
-    std::ranges::transform(forwardDiffError, centralDiffError.begin(), absFun);
-    std::ranges::transform(forwardDiffError, centralDiff4Error.begin(),absFun);
+    // Create some JSON objects to hold our data so we can serialize them to a file.
+    // This makes our calculations shareable with other programs like python for the
+    // purposes of graphing.
+    using json = nlohmann::json;
+    json data;
 
+    data["gridSize"] = grids;
+    data["forwardDiffError"]  = forwardDiffError;
+    data["centralDiffError"]  = centralDiffError;
+    data["centralDiff4Error"] = centralDiff4Error;
+
+    // Create a string that has our output path and output file in it.
+    // note that we could use a std path or some path lib to build a more
+    // robust and portable file writing behavior.
+    // Note: use a try catch block since IO operations can fail easily
+    try{
+        std::filesystem::create_directory("data");
+        std::string filename = "data/data.json";
+        std::ofstream ofs(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+
+        if(ofs.is_open()){
+            ofs << std::setw(2) << data << std::endl;
+        }
+        ofs.close();
+    } catch(std::exception &e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
 
     // create an ostream iterator and then copy our begin and end pointer
-    // to the anonymous ostream iterator.
+    // to the anonymous ostream iterator. This is a neat way to print to cout.
+    std::cout << "Grid Step Sizes" << std::endl;
+    std::ranges::copy(grids, std::ostream_iterator<double>(std::cout," "));
+    std::cout << std::endl << std::endl;
+
     std::cout << "1st Order Forward Diff Error" << std::endl;
     std::ranges::copy(forwardDiffError, std::ostream_iterator<double>(std::cout," "));
     std::cout << std::endl << std::endl;
